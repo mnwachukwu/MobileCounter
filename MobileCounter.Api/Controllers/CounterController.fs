899a87017a30f35d8ed8f95be1241a39193642d9
Matt N <matt.nwachukwu@gmail.com>
2023-08-29 01:49:37 -0400

    added project files

---
+++ MobileCounter.Api/Controllers/CounterController.fs
@@ -0,0 +1,28 @@
+﻿namespace MobileCounter.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open MobileCounter
+open System
+
+[<ApiController>]
+[<Route("[controller]")>]
+type CounterController(logger : ILogger<CounterController>) =
+    inherit ControllerBase()
+
+    static let counter =
+        { Date = DateTime.Now
+          Count = 0 }
+
+    [<HttpPost("/counter/update/{value}")>]
+    member this.Post(value) : IActionResult =
+        counter.Update value
+        (this.Ok counter) :> IActionResult
+
+    [<HttpGet>]
+    member this.Get() : IActionResult =
+        (this.Ok counter) :> IActionResult
---
+++ MobileCounter.Api/DailyCounter.fs
@@ -0,0 +1,10 @@
+﻿namespace MobileCounter
+
+open System
+
+type DailyCounter =
+    { Date: DateTime
+      mutable Count: int }
+
+    member this.Update(value) =
+        this.Count <- this.Count + value
